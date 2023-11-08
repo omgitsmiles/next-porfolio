21b6a2c128ebb53787f783de96c028f55934e0d2
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2023-11-07 21:36:32 -0500

    UGH

--- src/components/submit-btn.tsx
+++ src/components/submit-btn.tsx
@@ -1,10 +1,10 @@
 
 import React from 'react'
 import { FaPaperPlane } from 'react-icons/fa'
-import { experimental_useFormStatus as useFormStatus } from 'react-dom';
+import { experimental_useFormStatus } from 'react-dom';
 
 export default function SubmitBtn() {
-    const { pending } = useFormStatus()
+    const { pending } = experimental_useFormStatus()
 
   return (
     <button className='group flex items-center justify-center
