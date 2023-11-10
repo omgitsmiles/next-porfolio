b126e75bb1dc659adf3df694a34785b1214b50ad
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2023-11-10 10:52:57 -0500

    edited about me

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -18,11 +18,10 @@ export default function About() {
         <SectionHeading>About Me</SectionHeading>
         <p className='mb-3'>
             After 12 years in Account Management in the {" "}
-            <span className="font-medium">health care industry</span>, I decided to pursue my
-            passion for programming. I enrolled in a coding bootcamp and learned{" "}
-            <span className="font-medium">full-stack web development</span>.{" "}
-            <span className="italic">I am avid student of web development and continue to learn everyday.</span> The debugging, creative solutions, and collaboration is what I'm enjoying the most. I <span className="underline">love</span> the
-            feeling working together with others to build out creative passion projects. My core stack
+            <span className="font-medium">health care industry</span>, I decided to pursue my passion for programming.
+            At Flatiron School, I learned both front and back end technologies to become a{" "}
+            <span className="font-medium">full-stack developer</span>.{" "} 
+            <span className="italic">Since graduating, I've immesered myself in web dev and continue to learn everyday!</span> The thrill of debugging, the sweet satisfaction of finding those off-the-beaten-path solutions, and teaming up with fellow code enthusiasts to whip up some <span className="underline">seriously cool</span> passion projects. Web development isn't just a job; it's my playground for creative collaboration. My core stack
             is{" "}
             <span className="font-medium">
             React, Next.js, Node.js, Python, Flask, Ruby on Rails, and Typescript
@@ -34,11 +33,10 @@ export default function About() {
         </p>
         <p>
             <span className="italic">When I'm not coding</span>, I enjoy playing
-            spending time with my new born son and family, running, playing video games, and lounging with my dog. I look forward to{" "}
+            spending time with my new born son and family, getting together with friends for runs around NYC, video games, watching movies, and lounging with my dog. I look forward to{" "}
             <span className="font-medium">learning new things</span>. I am currently
             learning about{" "}
-            <span className="font-medium">Next.js version 14 and their partial rendering and deepening my knowledge of React.</span> I'm also
-            learning how to play the piano.
+            <span className="font-medium">Next.js 14 and their partial rendering and deepening my knowledge of React.</span>
         </p>
     </motion.section>
   )
