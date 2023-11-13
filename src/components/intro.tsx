f09a9f7e157fd118f570203194312fdb3b060245
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2023-11-13 09:04:41 -0500

    added tooltips

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -80,17 +80,20 @@ export default function Intro() {
                 Download CV <HiDownload className='opacity-60 group-hover:translate-y-1 transition'
                 />
             </a>
-            <a className='bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-110 hover:scale-[1.15] active:scale-[1.15] hover:text-gray-950 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60'
+            <a className='group bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-110 hover:scale-[1.15] active:scale-[1.15] hover:text-gray-950 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60'
                 href='https://linkedin.com/in/paolo-alberca' target='_blank'>
                 <BsLinkedin />
+                <span className="absolute top-10 scale-0 transition-all rounded bg-gray-800 p-2 text-xs text-white group-hover:scale-100">🤝 LinkedIn</span>
             </a>
-            <a className='bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60'
+            <a className='group bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60'
             href='https://github.com/omgitsmiles' target='_blank'>
                 <FaGithubSquare />
+                <span className="absolute top-10 scale-0 transition-all rounded bg-gray-800 p-2 text-xs text-white group-hover:scale-100">📂 Github</span>
             </a>
-            <a className='bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60'
+            <a className='group bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60'
             href='https://medium.com/@paolo.alberca' target='_blank'>
                 <FaMedium />
+                <span className="absolute top-10 scale-0 transition-all rounded bg-gray-800 p-2 text-xs text-white group-hover:scale-100">✍🏼 Blog</span>
             </a>
         </motion.div>
     </section>
--- src/components/theme-switch.tsx
+++ src/components/theme-switch.tsx
@@ -10,7 +10,7 @@ export default function ThemeSwitch() {
     const { theme, toggleTheme } = useTheme()
 
   return (
-    <button className='fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-90 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl
+    <button className='fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-25 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl
         rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transation-all dark:bg-gray-950'
         onClick={toggleTheme}>
             {
